ce0cc182badeabe3be0bf23e3addaa65c58c2bd9
d0422 <rlfehd2013@naver.com>
2023-01-08 01:24:14 +0900

    feat:add page first scene

--- notion/src/Page.js
+++ notion/src/Page.js
@@ -3,10 +3,24 @@ class Page {
     const mainUI = document.getElementById('main');
     const newPage = document.createElement('div');
     const newTitle = document.createElement('input');
+    const introduce = document.createElement('div');
+    const iconTemplate = document.createElement('div');
+    const noIconTemplate = document.createElement('div');
+
+    introduce.innerHTML =
+      'Enter 키를 눌러 빈 페이지로 시작하거나, ↕키로 원하는 템플릿을 선택하세요.';
     newTitle.placeholder = '제목 없음';
+    iconTemplate.innerHTML = '📑 아이콘이 있는 빈 페이지';
+    noIconTemplate.innerHTML = '📄 빈 페이지';
+    iconTemplate.classList.add('template');
+    noIconTemplate.classList.add('template');
+    introduce.classList.add('introduce');
     newPage.style.padding = '100px';
-    newPage.appendChild(newTitle);
     newTitle.classList.add('title');
+    newPage.appendChild(newTitle);
+    newPage.appendChild(introduce);
+    newPage.appendChild(iconTemplate);
+    newPage.appendChild(noIconTemplate);
     if (mainUI.hasChildNodes()) {
       mainUI.removeChild(mainUI.firstChild);
     }
