103691a8bfd2e8f88d7482a898ccf2e8496660e4
d0422 <rlfehd2013@naver.com>
2023-01-11 16:31:06 +0900

    refactor:Block 키보드 if문 객체로 처리

--- notion/src/Block.js
+++ notion/src/Block.js
@@ -1,50 +1,48 @@
+import { EMPTY_MESSAGE } from './const';
 import Dom from './Dom';
-
 class Block {
   constructor(main) {
-    const newBlock = new Dom(
-      'input',
-      'block',
-      '명령어 사용 시 "/"를 입력하세요'
-    );
+    const newBlock = new Dom('input', 'block', EMPTY_MESSAGE);
     newBlock.focus();
     main.append(newBlock);
-
-    newBlock.addEventListener('keydown', (event) => {
-      if (event.key === 'Enter') {
+    const keyDownEvent = {
+      Enter() {
         newBlock.placeholder = '';
         newBlock.onblur();
         new Block(main);
-      }
-      if (event.key === 'Backspace' && newBlock.value === '') {
+      },
+      Backspace() {
+        if (newBlock.value !== '') return;
         main.remove(newBlock);
-        if (main.childElementCount != 2) {
-          main.lastChild.placeholder = '명령어 사용 시 " / "를 입력하세요';
-        }
+        if (main.childElementCount != 2)
+          main.lastChild.placeholder = EMPTY_MESSAGE;
         setTimeout(() => main.lastChild.focus(), 0);
-      }
-      if (event.key === '/') {
-        console.log('/눌러짐');
-      }
-      if (event.key === 'ArrowUp') {
+      },
+      ArrowUp() {
         newBlock.placeholder = '';
         setTimeout(() => {
           newBlock.previousSibling.focus();
-          newBlock.previousSibling.placeholder =
-            '명령어 사용 시 " / "를 입력하세요';
+          newBlock.previousSibling.placeholder = EMPTY_MESSAGE;
         }, 0);
-      }
-      if (event.key === 'ArrowDown') {
+      },
+      ArrowDown() {
         if (newBlock.nextSibling) {
           newBlock.placeholder = '';
           setTimeout(() => {
             newBlock.nextSibling.focus();
-            newBlock.nextSibling.placeholder =
-              '명령어 사용 시 " / "를 입력하세요';
+            newBlock.nextSibling.placeholder = EMPTY_MESSAGE;
           }, 0);
         }
+      },
+    };
+
+    newBlock.addEventListener('keydown', (event) => {
+      keyDownEvent[event.key]();
+      if (event.key === '/') {
+        console.log('/눌러짐');
       }
     });
+
     newBlock.addEventListener('keyup', (event) => {
       if (event.key === ' ' && newBlock.value === '# ') {
         newBlock.addClass('h1');
